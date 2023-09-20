b71e7d6ff0822e4c27451f94e533e6e13f3118dc
202230142 <202230142@PO-21493>
2023-09-20 13:13:28 -0400

    Étape 1

--- JeuGladiateurs/src/personnages/Personnage.java
+++ JeuGladiateurs/src/personnages/Personnage.java
@@ -27,29 +27,87 @@ public class Personnage {
 
     public Personnage() {
         // TODO : Constructeur SANS paramètres qui initialise à ZÉRO ou à ""
+        this.nom = "";
+        this.pointsDeVie = 0;
+        this.valeurMaxAttaque = 0;
+        this.valeurDefense = 0;
+        this.initiative = 0;
     }
+
     // </editor-fold>
+    public String getNom() {
+        return nom;
+        // TODO : Afficher les infos du personnage, tel que montré dans l'énoncé
+    }
+
+    public int getPointsDeVie() {
+        return pointsDeVie;
+    }
+
+    public int getValeurMaxAttaque() {
+        return valeurMaxAttaque;
+    }
+
+    public int getValeurDefense() {
+        return valeurDefense;
+    }
 
     // **************************************************************************
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Getters et setters">
     // TODO : Les getters
-    // TODO : Les setters
-    // </editor-fold>
+    public int getInitiative() {
+        return initiative;
+    }
 
+    public void setNom(String nom) {
+        this.nom = nom;
+        // TODO : Afficher les infos du personnage, tel que montré dans l'énoncé
+    }
+
+    public void setPointsDeVie(int pointsDeVie) {
+        this.pointsDeVie = pointsDeVie;
+    }
+
+    public void setValeurMaxAttaque(int valeurMaxAttaque) {
+        this.valeurMaxAttaque = valeurMaxAttaque;
+    }
+
+    public void setValeurDefense(int valeurDefense) {
+        this.valeurDefense = valeurDefense;
+    }
+
+    // TODO : Les setters
+    public void setInitiative(int initiative) {
+        this.initiative = initiative;
+    }
+
+    // </editor-fold>
     // **************************************************************************
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Mécanique de jeu">
     public void afficherInfosPersonnage() {
         // TODO : Afficher les infos du personnage, tel que montré dans l'énoncé
-    }
+        System.out.println(" ");
+        System.out.println(nom);
+        System.out.println("Attaque : " + valeurMaxAttaque);
+        System.out.println("Defense : " + valeurDefense);
+        System.out.println("Points de vie : " + pointsDeVie);
+        System.out.println("Initiative : " + initiative);
 
+        if (pointsDeVie <= 0) {
+            System.out.println("statut : Mort ");
+        } else {
+            System.out.println("statut : Vivant ");
+        }
+    }
+    
     private int attaqueCalcul() {
         // TODO : Retourner la valeur de l'attaque du personnage.
         // Cette valeur est trouvée aléatoirement et doit se situer entre ZÉRO et valeurMaxAttaque.
-        return 0;
+       return 0;
     }
 
     public void frapperPersonnage(Personnage personnageCible) {
