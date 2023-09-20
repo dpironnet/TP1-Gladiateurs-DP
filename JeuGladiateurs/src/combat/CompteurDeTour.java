8495adff7ed0b3a91c032e9fe52abf0c1e4a819b
202230142 <202230142@PO-21493>
2023-09-20 15:21:33 -0400

    Étape 1

--- JeuGladiateurs/src/combat/CompteurDeTour.java
+++ JeuGladiateurs/src/combat/CompteurDeTour.java
@@ -7,6 +7,8 @@ public class CompteurDeTour {
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Attributs">
     // TODO : Mettre vos attributs ici
+    int cptrTour;
+    
     // </editor-fold>
     
     // **************************************************************************
@@ -15,6 +17,7 @@ public class CompteurDeTour {
     // <editor-fold defaultstate="collapsed" desc="Constructeurs et initialisation">
     public CompteurDeTour() {
         // TODO : Constructeur SANS paramètres qui initialise un compteur de tour (attribut) à 1
+        this.cptrTour = 1;
     }
     // </editor-fold>
 
@@ -32,10 +35,23 @@ public class CompteurDeTour {
     // <editor-fold defaultstate="collapsed" desc="Mécanique de jeu">
     public void augmenteTour() {
         // TODO : Incrémenter le compteur de tour
+        this.cptrTour++;
+    }
+
+    public void setCptrTour(int cptrTour) {
+        this.cptrTour = cptrTour;
+    }
+
+    public int getCptrTour() {
+        return cptrTour;
     }
 
     public void afficheTour() {
         // TODO : Afficher le tour actuel, tel que montré dans l'énoncé
+        System.out.println("-----------------");
+        System.out.println("TOUR ACTUEL : " + cptrTour);
+        System.out.println("-----------------");
+        
     }
     // </editor-fold>
 }
--- JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
+++ JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
@@ -14,8 +14,8 @@ public class JeuGladiateurs {
     // <editor-fold defaultstate="expanded" desc="Instanciation des objets">
     CompteurDeTour tour = new CompteurDeTour();
     AffichageEcran affichage = new AffichageEcran();
-    Personnage Bob = new Personnage("Bob le malchanceux", 70,15,15,15);
-    Personnage Igor = new Personnage("igor l'empaleur", 100,25,5,30);
+    Personnage Bob = new Personnage("Bob le malchanceux", 15,15,70,15);
+    Personnage Igor = new Personnage("igor l'empaleur", 25,5,100,30);
     // </editor-fold>
 
     // **************************************************************************
@@ -26,6 +26,9 @@ public class JeuGladiateurs {
     Bob.afficherInfosPersonnage();
     Igor.afficherInfosPersonnage();
     // TODO : Afficher le message du début du combat
+    affichage.afficherDebutCombat();
+    
+
     // </editor-fold>
 
     // **************************************************************************
